25356bce45fa6122238a74cb7142c0bbf7cbcafe
Josh <josh.doelling@gmail.com>
2023-11-13 15:03:48 -0500

    DropDown

--- canvas/src/components/Sidebar.jsx
+++ canvas/src/components/Sidebar.jsx
@@ -1,11 +1,32 @@
-// src/components/Sidebar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidebar = ({ onPageChange }) => {
+  const [isMathDropdownOpen, setMathDropdownOpen] = useState(false);
+
+  const handleMathClick = () => {
+    setMathDropdownOpen(!isMathDropdownOpen);
+  };
+
+  const handleMathItemClick = (subpage) => {
+    onPageChange(`Math/${subpage}`);
+    setMathDropdownOpen(false);
+  };
+
   return (
     <nav className="sidebar">
       <ul>
-        <li onClick={() => onPageChange('classSelection')}>Class Selection</li>
+        <li onClick={handleMathClick} className={isMathDropdownOpen ? 'active' : ''}>
+          Math
+          {isMathDropdownOpen && (
+            <ul className="dropdown-list">
+              <li onClick={() => handleMathItemClick('Modules')}>Modules</li>
+              <li onClick={() => handleMathItemClick('Assignments')}>Assignments</li>
+              <li onClick={() => handleMathItemClick('Syllabus')}>Syllabus</li>
+              <li onClick={() => handleMathItemClick('Zoom')}>Zoom</li>
+              {/* Add more items as needed */}
+            </ul>
+          )}
+        </li>
         <li onClick={() => onPageChange('grades')}>Grades</li>
         <li onClick={() => onPageChange('calendar')}>Calendar</li>
       </ul>
