01d47035fda0c78dc8773a6421f2b585b600fe66
Josh <josh.doelling@gmail.com>
2023-11-13 19:37:55 -0500

    AssignmentsKindaGood

--- canvas/src/components/Assignments.jsx
+++ canvas/src/components/Assignments.jsx
@@ -1,96 +1,95 @@
 // Assignments.jsx
 import React, { useState } from 'react';
 import './Assignments.css';
-import MazePopup from './MazePopup'; // Adjust the path as needed
+import MazePopup from './MazePopup';
+import SubmissionPopup from './SubmissionPopup'; // Import the new component
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown, faCheck  } from '@fortawesome/free-solid-svg-icons';
+import { faArrowDown, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const Assignments = () => {
-    
-    const [showMazePopup, setShowMazePopup] = useState(false);
+  const [showMazePopup, setShowMazePopup] = useState(false);
+  const [showSubmissionPopup, setShowSubmissionPopup] = useState(false); // New state for SubmissionPopup
 
-    const handleDownload = () => {
-        // Show the maze popup
-        setShowMazePopup(true);
-      };
+  const handleDownload = () => {
+    setShowMazePopup(true);
+  };
 
-    const handleCloseMazePopup = () => {
-        // Close the maze popup
-        setShowMazePopup(false);
-      };
+  const handleCloseMazePopup = () => {
+    setShowMazePopup(false);
+  };
 
-    const handleSubmit = (itemName) => {
-        // Implement the logic for submitting the item
-        console.log(`Submitting ${itemName}`);
-    };
+  const handleSubmit = () => {
+    setShowSubmissionPopup(true); // Show the SubmissionPopup
+  };
 
-    const handleMazeSolve = () => {
-        // Logic to handle what happens after solving the maze
-        console.log('Maze solved!');
-      };
+  const handleCloseSubmissionPopup = () => {
+    setShowSubmissionPopup(false); // Close the SubmissionPopup
+  };
 
+  const handleMazeSolve = () => {
+    console.log('Maze solved!');
+  };
 
-    return (
-        <div className="assignments-container">
-            <div className="assignment-section">
-                <h2>Homework</h2>
-                <ul>
-                    <li>
-                        Assignment 1
-                        <span className="due-date">Due: October 15, 2023</span>
-                        <button onClick={() => handleDownload('Assignment 1')} className="download-button">
-                            <FontAwesomeIcon icon={faArrowDown} />
-                        </button>
-                        <button onClick={() => handleSubmit('Assignment 1')} className="submit-button">
-                            <FontAwesomeIcon icon={faCheck} />
-                        </button>
-                    </li>
-                </ul>
-            </div>
+  return (
+    <div className="assignments-container">
+      <div className="assignment-section">
+        <h2>Homework</h2>
+        <ul>
+          <li>
+            Assignment 1
+            <span className="due-date">Due: October 15, 2023</span>
+            <button onClick={() => handleDownload('Assignment 1')} className="download-button">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </button>
+            <button onClick={handleSubmit} className="submit-button">
+              <FontAwesomeIcon icon={faCheck} />
+            </button>
+          </li>
+        </ul>
+      </div>
 
+      <div className="assignment-section">
+        <h2>Exams</h2>
+        <ul>
+          <li>
+            Midterm Exam
+            <span className="due-date">Due: November 5, 2023</span>
+          </li>
+          {/* Add more exam items */}
+        </ul>
+      </div>
 
-            <div className="assignment-section">
-                <h2>Exams</h2>
-                <ul>
-                    <li>
-                        Midterm Exam
-                        <span className="due-date">Due: November 5, 2023</span>
-                    </li>
-                    {/* Add more exam items */}
-                </ul>
-            </div>
+      <div className="assignment-section">
+        <h2>Quizzes</h2>
+        <ul>
+          <li>
+            Quiz 1
+            <span className="due-date">Due: September 30, 2023</span>
+          </li>
+          {/* Add more quiz items */}
+        </ul>
+      </div>
 
-            <div className="assignment-section">
-                <h2>Quizzes</h2>
-                <ul>
-                    <li>
-                        Quiz 1
-                        <span className="due-date">Due: September 30, 2023</span>
-                    </li>
-                    {/* Add more quiz items */}
-                </ul>
-            </div>
+      <div className="assignment-section">
+        <h2>Labs</h2>
+        <ul>
+          <li>
+            Lab 1
+            <span className="due-date">Due: October 8, 2023</span>
+            <button onClick={() => handleDownload('Lab 1')} className="download-button">
+              <FontAwesomeIcon icon={faArrowDown} />
+            </button>
+            <button onClick={handleSubmit} className="submit-button">
+              <FontAwesomeIcon icon={faCheck} />
+            </button>
+          </li>
+        </ul>
+      </div>
 
-            <div className="assignment-section">
-                <h2>Labs</h2>
-                <ul>
-                    <li>
-                        Lab 1
-                        <span className="due-date">Due: October 8, 2023</span>
-                        <button onClick={() => handleDownload('Lab 1')} className="download-button">
-                            <FontAwesomeIcon icon={faArrowDown} />
-                        </button>
-                        <button onClick={() => handleSubmit('Assignment 1')} className="submit-button">
-                            <FontAwesomeIcon icon={faCheck} />
-                        </button>
-                    </li>
-                </ul>
-            </div>
-            {showMazePopup && (
-                <MazePopup onClose={handleCloseMazePopup} onSolve={handleMazeSolve} />
-            )}
-        </div>
-    );
+      {showMazePopup && <MazePopup onClose={handleCloseMazePopup} onSolve={handleMazeSolve} />}
+      {showSubmissionPopup && <SubmissionPopup onClose={handleCloseSubmissionPopup} />}
+    </div>
+  );
 };
 
 export default Assignments;
